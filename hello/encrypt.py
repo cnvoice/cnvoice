dc84f178e06def6ded7f681565e62fb559a6e5a0
Nanshu Wang <nanshuwang@fb.com>
2020-02-10 18:51:48 -0800

    ui optimize & add fix on begin and end of sentence

--- hello/encrypt.py
+++ hello/encrypt.py
@@ -1,11 +1,14 @@
 import random
+
+SEPERATOR = set([" ", "，", "。", ",", ";", ",", "?", ".", "？", "；"] \
+    + ["“", "”", "#","、", "【", "】", "《", "》", "[", "]", '"', "'"] \
+    + ["-", "-", "_", "\n", "\t"])
+
 def is_ascii(c):
     return ord(c) < 128
     
 def is_seperator(c):
-    return c in [" ", "，", "。", ",", ";", ",", "?", ".", "？", "；"] \
-    + ["“", "”", "#","、", "【", "】", "《", "》", "[", "]", '"', "'"] \
-    + ["-", "-", "_", "\n"]
+    return c in SEPERATOR
     
 def tokenize(src_txt):
     token_list = []
@@ -25,14 +28,22 @@ def tokenize(src_txt):
 def reorder(token_list):
     n_grams = [2, 3]
     i = 0
-    token_list_reordered = []
-    while (i<len(token_list)):
+    if len(token_list) <= 3:
+        return token_list
+
+    token_list_reordered = [token_list[0]]
+    middle_token_list = token_list[1:-1]
+
+    while (i<len(middle_token_list)):
         n_gram = random.choice(n_grams)
-        j = min(i+n_gram, len(token_list))
-        n_gram = token_list[i:j]
+        j = min(i+n_gram, len(middle_token_list))
+        n_gram = middle_token_list[i:j]
         random.shuffle(n_gram)
         token_list_reordered.extend(n_gram)
         i = j
+
+    token_list_reordered.append(token_list[-1])
+
     return token_list_reordered
     
 def sentencize(src_txt):
